d757db3e07455426eab136af4dbf452b9af1738e
yangzhiqiang54 <yzq_goodjob@126.com>
2022-06-17 16:19:10 +0800

    scl.c: add presolve

--- scl.c
+++ scl.c
@@ -34,97 +34,246 @@ static uint16_t pre_top = 0, main_top = 0, temp_top = 0;
 
 /**** end 声明函数 ****/
 
+/* 模块初始化函数 */
+static void scl_init(void) {
+    memset(scl_stack_pre, 0, sizeof(scl_stack_pre));
+    memset(scl_stack_main, 0, sizeof(scl_stack_main));
+    memset(scl_stack_temp, 0, sizeof(scl_stack_temp));
+    pre_top = 0;
+    main_top = 0;
+    temp_top = 0;
+}
+
 /* 获取入栈数 */
-static uint16_t get_stack_top(scl_stack_typedef *ps) {
-    if(ps == scl_stack_pre) return pre_top;
-    else if(ps == scl_stack_main) return main_top;
-    else if(ps == scl_stack_temp) return temp_top;
+static uint16_t* get_stack_top(scl_stack_typedef *ps) {
+    if(ps == scl_stack_pre) return &pre_top;
+    else if(ps == scl_stack_main) return &main_top;
+    else if(ps == scl_stack_temp) return &temp_top;
     else return 0;
 }
 
-/* 入栈函数 */
+/* 入栈函数 @t:1值 2运算符*/
 static void in_stack(scl_stack_typedef *pstack, float v, uint8_t t, char s) {
-    uint16_t num = get_stack_top(pstack);
-    pstack[num].val = v;
-    pstack[num].type = t;
-    pstack[num].symbol = s;
-    num++;
+    uint16_t *pnum = get_stack_top(pstack);
+    pstack[*pnum].val = v;
+    pstack[*pnum].type = t;
+    pstack[*pnum].symbol = s;
+    (*pnum)++;
 }
 
 /* 出栈函数 */
 static scl_stack_typedef out_stack(scl_stack_typedef *pstack) {
-    uint16_t num = get_stack_top(pstack);
-    if(num > 0) return pstack[num-1];
+    uint16_t *pnum = get_stack_top(pstack);
+    if((*pnum) > 0) {
+        (*pnum) -= 1;
+        return pstack[(*pnum)-1];
+    }
     else pstack[0];
+}
+
+/* 指定位置取栈函数 */
+static scl_stack_typedef out_stack_index(scl_stack_typedef *pstack, int num) {
+    
 }
 
 /* 打印栈全部内容 */
 static void print_stack(scl_stack_typedef *pstack) {
     int i=0;
-    uint16_t num = get_stack_top(pstack);
+    uint16_t num = *get_stack_top(pstack);
 
-    if(pstack == scl_stack_pre) printf(" #pre stakc print: \n   num    type    val    symbol");
-    else if(pstack == scl_stack_main) printf(" #main stakc print: \n");
-    else if(pstack == scl_stack_temp) printf(" #temp stakc print: \n");
+    if(pstack == scl_stack_pre) printf("\n#pre stakc print: \nnum  type  val    symbol\n");
+    else if(pstack == scl_stack_main) printf("\n#main stakc print: \nnum  type  val    symbol\n");
+    else if(pstack == scl_stack_temp) printf("\n#temp stakc print: \nnum  type  val    symbol\n");
     else return;
 
     for(i=0; i<num; i++) {
-        printf("   %d    %d    %f    %c \n", i+1, pstack[i].type, pstack[i].val, pstack[i].symbol);
+        printf("%-4d %-4d  %4.3f  %c\n", i, pstack[i].type, pstack[i].val, pstack[i].symbol);
     }
 }
+
+/* 从字符串指定位置取出连续的数字，返回整形 */
+static int get_num(char *pch) {
+    int i=0,temp=0;
+    if(pch[0] < '1' || pch[0] > '9') {
+        return -1;
+    }
+    temp = pch[0] - 0x30;
+    i++;
+    while(pch[i]>='0' && pch[i]<='9') {
+        temp = temp * 10 + (pch[i] - 0x30);
+        i++;
+    }
+    return temp;
+}
+
+/* 获取一个整形数字的位数 */
+static int get_num_digit(int num) {
+    uint8_t pos=0;
+    if(num == 0) {
+        pos = 0;
+    }
+    pos = 1;
+    while(num / 10) {
+        pos++;
+        num /= 10;
+    }
+    return pos;
+}
+
+const float basedata[20] = {1,2,3,4,5,6,7,8,9,10,1.1,1.2,1.3,1.4,1.5,1.6,1.7,1.8,1.9};
 
 /* 
     对输入字符串进行预处理 
     1.判断必须以[]头尾括起来
     2.删除空格等无关字符
+    3.连续符号，报错退出
     @str:输入的运算字符串
     @return:预处理结果 0成功 -1失败
 */
 static int scl_prosolv(char* str) {
-    int i=0, stlen=0;
-    float f1 = 0;
-    uint8_t weishu = 0;
+    int res = 0, i=0, j=0, stlen=0;
+    scl_stack_typedef st_scl_temp;
 
-    memset(scl_stack_pre, 0, sizeof(scl_stack_pre));
-    memset(scl_stack_main, 0, sizeof(scl_stack_main));
-    memset(scl_stack_temp, 0, sizeof(scl_stack_temp));
-
+    /* 判断是否由 [ ] 包括 */
     stlen = strlen(str);
-    if(stlen <=2 || str[0] != '[' || str[stlen-1] != ']') 
-        return -1;
+    if(stlen <=2 || str[0] != '[' || str[stlen-1] != ']') {
+        res = -1;
+        goto EXIT;
+    }
 
-    /* 将输入字符格式化到预处理栈中 */
-    for(i=1; i<stlen-1; i++) { //跳过'['和']'
-        if(str[i] >= '0' && str[i] <= '9') {
-
+    /* 去除无效字符：空格 */
+    for(i=0; i<stlen; i++) {
+        if(str[i] == ' ') {
+            memcpy(str+i, str+i+1, stlen-i);
+            j++;
         }
     }
+    stlen -= j; //去除无效字符后的字符串长度
+
+    /* 判断是否有连续符号 */
+    for(i=0; i<stlen-1; i++) {
+        if(str[i]<'0' || str[i]>'9') {
+            if(str[i+1]<'0' || str[i+1]>'9') {
+                if(str[i+1] != '#') {
+                    res = -1;
+                    goto EXIT;
+                }
+            }
+        }
+    }
+        
+    /* 将输入字符格式化到预处理栈中 */
+    int numid, numdigit;
+    for(i=1; i<stlen-1; i++) { //跳过'['和']'
+        switch(str[i]) {
+            case '#':
+                numid = get_num(&str[i+1]);
+                /* #后面id值错误 */
+                if(numid <= 0) {
+                    i += 1;
+                    break;
+                }
+                /* 入栈 */
+                in_stack(scl_stack_pre, basedata[numid-1], 1, 0);
+                /* 字符串向后偏移 位数 */
+                numdigit = get_num_digit(numid);
+                i += numdigit;
+            break;
+
+            case '+':
+            case '-':
+            case '*':
+            case '/':
+            case '(':
+            case ')':
+            case 'b':
+            case 'f':
+            case '~':
+            case '^':
+                /* 运算符号入栈 */
+                in_stack(scl_stack_pre, 0, 2, str[i]);
+            break;
+
+            default:break;
+        }
+    }
+
+    EXIT:
+    return res;
+}
+
+/*
+    进行出栈操作并进行算数运算
+*/
+static void scl_cacl(void) {
+    int i, pre_statck_num;
+    scl_stack_typedef st_cur, st_nextcur;
+    float cval=0;
+
+    /* 从预处理栈中出个取出进行计算（从左往右） */
+    pre_statck_num = pre_top;
+    for(int i=0; i<pre_statck_num; i++) {
+        st_cur = scl_stack_pre[i];
+        if(st_cur.type == 1) {
+            /* 数值先入栈，等待运算符号 */
+            in_stack(scl_stack_main, st_cur.val, st_cur.type, st_cur.symbol);
+        }
+        else if(st_cur.type == 2) {
+            /* 运算符号 */
+            /* +和- 先入栈，最后统一计算 */
+            if(st_cur.symbol == '+' || st_cur.symbol == '-') {
+                in_stack(scl_stack_main, st_cur.val, st_cur.type, st_cur.symbol);
+            }
+            else if(st_cur.symbol == '*' || st_cur.symbol == '/') {
+                /* *和/后面是数值，直接运算 */
+                if(scl_stack_pre[i+1].type == 1) {
+                    st_nextcur = scl_stack_pre[i+1];
+                    if(st_cur.symbol == '*') {
+                        cval = st_cur.val * st_nextcur.val;
+                    }else if(st_cur.symbol == '/') {
+                        cval = st_cur.val / st_nextcur.val;
+                    }
+                    /* 将*和/计算后的结果入栈 */
+                    in_stack(scl_stack_main, cval, 1, 0);
+                }
+                /* *和/后面是运算符，先把这当前符号入栈 */
+                else {
+                    in_stack(scl_stack_main, st_cur.val, st_cur.type, st_cur.symbol);
+                }
+            }
+        }
+    }
+
 
 }
 
 
-
-
-
-
-
-
-char srcstr[100] = {"#1+#2*#3-(#4-#5)"};
+char srcstr[100] = {"[#15 + #2*#3 -(#4-#5)]"};
+char srcstr2[100] = {"[#1 + #2 * #3]"};
 float exval[20] = {0};
 void main(char argc, char* agrv[]) {
-
-    int i=0;
-
     printf("  second calc module! \n\n");
 
+    int i=0;
+    
     /* 模拟采集点位数据 */
     for(i=0; i<20; i++) {
         exval[i] = i+1;
-        printf("#%d=%f ", i+1, exval[i]);
+        if(i % 5 == 0) printf("\n");
+        printf("#%d=%.3f ", i+1, exval[i]);
     }
 
-    printf("\n\n src_str: %s \n", srcstr);
-    scl_prosolv(srcstr);
+    // scl_init();
+    // printf("\n\n src_str: %s \n", srcstr);
+    // scl_prosolv(srcstr);
+
+    scl_init();
+    printf("\n\n src_str: %s \n", srcstr2);
+    if(scl_prosolv(srcstr2) < 0) {
+        printf("calc string error!(%s)\n", srcstr2);
+    }
+
+
     print_stack(scl_stack_pre);
 }
 
